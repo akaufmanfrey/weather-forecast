7e43215b7d33a3c26f62ad5fadcfdc8657ca12df
akaufmanfrey <akaufmanfrey@gmail.com>
2024-07-18 13:33:24 -0400

    added README and commented code

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # weather-forecast
 An app for viewing 5 day weather forecasts
+
+## Description
+This project was made to build familiarity with making API calls. It uses the openweathermap APIs to find coordinates of a searched city and weather info for that city.
+
+[A link to the website](https://akaufmanfrey.github.io/weather-forecast/)
+![A screenshot of the website](./assets/forecast_screenshot.PNG)
+
+## Installation
+
+N/A
+
+## Usage
+
+A tool for a user to search a city and see the 5 day weather forecast. Their search history is stored locally and displayed on the page as clickable buttons which search for the corresponding city when clicked.
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ const weatherDisplayElement = $('#weather');
 const apiKey = 'f1d985f2f05b0ca0bb78b860038d22dc'
 
 function initializePage() {
+    // generate search buttons based on local storage
     const cityHistory = readCitiesFromStorage();
     if (cityHistory) {
         cityHistory.forEach(generateHistoryButton);
@@ -11,6 +12,7 @@ function initializePage() {
 }
 
 function generateHistoryButton(city) {
+    // generate a single button and append it to the search history column
     const cityButton = $('<button>');
     cityButton.addClass('bg-primary-subtle py-2 search-history block my-2 w-100');
     cityButton.text(city);
@@ -19,13 +21,17 @@ function generateHistoryButton(city) {
 
 function handleSubmit(e) {
     e.preventDefault();
+    // grab user's input
     const cityInputEl = $('#city');
     const cityInput = cityInputEl.val().trim();
+    // if non-empty, make a call to the geo api to get city coordinates
     if (cityInput) {
         const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${apiKey}`
         fetch(apiUrl).then(function (response) {
             if (response.ok) {
                 console.log(response);
+
+                // parse response into a JS object
                 response.json().then(function (data) {
                     if (data.length !== 0) {
                         fetchWeatherInfo(data[0]);
@@ -41,19 +47,25 @@ function handleSubmit(e) {
 }
 
 function fetchWeatherInfo(city) {
+    // using the city's coordinates, make a call to the weather api
     const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${apiKey}`
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             console.log(response);
             response.json().then(function(data) {
+                // get search history and check if the current city is already in local storage
                 const cityArray = readCitiesFromStorage();
                 if (!(cityArray.includes(city.name))) {
-                cityArray.push(city.name);
-                generateHistoryButton(city.name);
-                localStorage.setItem('cities', JSON.stringify(cityArray));
+                    // if not included, add city name to local storage array
+                    cityArray.push(city.name);
+                    generateHistoryButton(city.name);
+                    localStorage.setItem('cities', JSON.stringify(cityArray));
                 }
+                //clear display and generate new city cards
                 weatherDisplayElement.empty();
+                //give first card extra width
                 createWeatherCard(data.list[0], city.name, 12);
+                // generate header for 5 day forecast cards
                 const title = $('<h2>');
                 title.text('5 day forecast:');
                 title.addClass('mx-3 w-100');
@@ -83,21 +95,25 @@ function readCitiesFromStorage() {
 }
 
 function createWeatherCard(data, name='', width=2) {
+    // create container for all weather info
     const mainCard = $('<div>');
     mainCard.addClass(`card bg-primary text-white my-3 mx-3 col-${width}`);
     const cardDate = $('<div>');
     cardDate.addClass('card-header');
-    if (width=100) {
+    // check if the card being created is the current forecast, add the city name in front of the date
+    if (width=12) {
         cardDate.text(name + ' (' + data.dt_txt.slice(0, 10) + ')');
     } else {
         cardDate.text(data.dt_txt.slice(0, 10));
     }
+    // create img for weather conditions based on the icons stored on openweathermap
     const cardCond = $('<img>');
     cardCond.attr('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
     cardCond.attr('height', '50px');
     cardCond.attr('width', '50px');
     const cardTemp = $('<p>');
     cardTemp.addClass('card-text');
+    // since the api call is made with units=metric, the temp is in celsius, wind speed is in m/s
     cardTemp.text('Temperature: ' + data.main.temp + ' \u00B0C');
     const cardWind = $('<p>');
     cardWind.addClass('card-text');
@@ -105,6 +121,7 @@ function createWeatherCard(data, name='', width=2) {
     const cardHumid = $('<p>');
     cardHumid.addClass('card-text');
     cardHumid.text('Humidity: '+ data.main.humidity +'%');
+    // all all content to the card and append the card to the page
     mainCard.append(cardDate);
     mainCard.append(cardCond);
     mainCard.append(cardTemp);
@@ -113,6 +130,7 @@ function createWeatherCard(data, name='', width=2) {
     weatherDisplayElement.append(mainCard);
 
 }
+// event handler for clicking on search history buttons
 searchHistory.on('click', '.search-history', function (event) {
     $('#city').val($(event.target).text());
     submitButton.click();
